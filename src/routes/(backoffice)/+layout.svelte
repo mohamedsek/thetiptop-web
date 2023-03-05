d32967cb2f48ddf8cc59a64eb11cba764ba4703c
ASMA TIMZIT <atimzit@gmail.com>
2023-03-05 21:00:06 +0100

    app-structure: add backoffice structure

---
+++ src/routes/(backoffice)/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import BoFooter from "$lib/components/backoffice/BoFooter.svelte";
+    import BoHeader from "$lib/components/backoffice/BoHeader.svelte";
+</script>
+
+
+<BoHeader></BoHeader>
+    <slot/>
+<BoFooter></BoFooter>
