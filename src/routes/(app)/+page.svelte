1309fb6601e9b2d2244374e78c4357f0fbd538f9
soukaina <soukaina.bairouk@gmail.com>
2023-03-06 14:15:27 +0100

    Fix unit/E2E tests

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Page d'accueil</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>Home Page</h1>
+	<h1>Page d'accueil</h1>
 </section>
 
 <style>
---
+++ src/unit-tests/empty.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('empty test', () => {
+	it('empty test 1', () => {
+		expect(1 === 1).toBe(true);
+	});
+});
