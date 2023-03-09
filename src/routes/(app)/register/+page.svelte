afe80a73c365623e93fcb1259bfe244f536847dc
soukaina <soukaina.bairouk@gmail.com>
2023-03-09 16:50:47 +0100

    styling login & register pages

--- src/routes/(app)/register/+page.svelte
+++ src/routes/(app)/register/+page.svelte
@@ -3,6 +3,8 @@
 	import { messageErrors } from '$services/formValidation';
 	import { field, form } from 'svelte-forms';
 	import { between, email as emailValidator, required } from 'svelte-forms/validators';
+	import AuthSocialNetworks from '$lib/components/app/auth/AuthSocialNetworks.svelte';
+	// /home/user/work/new/thetiptop-web/src/lib/components/app/auth/AuthSocialNetworks.svelte
 
 	let backValidationErrors = {};
 
@@ -81,131 +83,172 @@
 	<meta name="description" content="Register Page" />
 </svelte:head>
 
-<div class="mb-4 mt-3 social-netowrks">
-	<a href="{import.meta.env.VITE_API_BASE_URL}/oauth2/authorize/google">Auth with Google</a>
-	<br />
-	<a href="{import.meta.env.VITE_API_BASE_URL}/oauth2/authorize/facebook"
-		>Auth with Facebook (config a ajouté coté back)</a
-	>
+<div class="container row  border rounded m-auto p-0">
+	<div class="left-banner  col-5 rounded-start d-none d-lg-flex ps-4">
+		<div class="lh-lg align-middle fs-5">
+			Bienvenue ! <br />
+			Veuillez vous inscrire en utilisant un compte Facebook , Google ou LinkedIn sinon vous pouvez utiliser
+			votre adresse mail.
+		</div>
+	</div>
+	<div class="form-container col-12 col-md-12 col-lg-7 ">
+		<h1 class="text-center fw-bold fs-3 mt-4">Inscription</h1>
+		<div class="mb-4 mt-3 social-netowrks social-icons d-flex justify-content-center ">
+			<AuthSocialNetworks />
+		</div>
+		<h6 class="text-center fw-bold fs-4">Ou utiliser votre adresse mail</h6>
+		<form on:submit={handleSubmit} class="p-4">
+			<div class="mb-3 ">
+				<div class="form-floating">
+					<input
+						type="text"
+						class="form-control {!$firstName.valid && 'is-invalid'}"
+						id="floatingFirstName"
+						placeholder="Prénom"
+						bind:value={$firstName.value}
+					/>
+					<label class={!$firstName.valid && 'text-danger'} for="floatingFirstName">
+						{$registrationForm.hasError('firstName.required')
+							? messageErrors.firstName.required
+							: ''}
+						{$registrationForm.hasError('firstName.alpha') ? messageErrors.firstName.alpha : ''}
+						{$registrationForm.hasError('firstName.between') ? messageErrors.firstName.between : ''}
+						{!$registrationForm.hasError('firstName.required') &&
+						!$registrationForm.hasError('firstName.alpha') &&
+						!$registrationForm.hasError('firstName.between')
+							? 'Prénom'
+							: ''}
+					</label>
+				</div>
+			</div>
+			<div class="mb-3">
+				<div class="form-floating">
+					<input
+						type="text"
+						class="form-control {!$lastName.valid && 'is-invalid'}"
+						id="floatingLastName"
+						placeholder="Nom"
+						bind:value={$lastName.value}
+					/>
+					<label class={!$lastName.valid && 'text-danger'} for="floatingLastName">
+						{$registrationForm.hasError('lastName.required') ? messageErrors.lastName.required : ''}
+						{$registrationForm.hasError('lastName.alpha') ? messageErrors.lastName.alpha : ''}
+						{$registrationForm.hasError('lastName.between') ? messageErrors.lastName.between : ''}
+						{!$registrationForm.hasError('lastName.required') &&
+						!$registrationForm.hasError('lastName.alpha') &&
+						!$registrationForm.hasError('lastName.between')
+							? 'Nom'
+							: ''}
+					</label>
+				</div>
+			</div>
+			<div class="mb-3">
+				<div class="form-floating">
+					<input
+						type="email"
+						class="form-control {!$email.valid || backValidationErrors.email ? 'is-invalid' : ''}"
+						id="floatingEmail"
+						placeholder="mail@exemple.fr"
+						bind:value={$email.value}
+						on:input={() => removeBackError('email')}
+					/>
+					<label
+						class={(!$email.valid || backValidationErrors.email) && 'text-danger'}
+						for="floatingEmail"
+					>
+						{backValidationErrors.email ? backValidationErrors.email : ''}
+						{!backValidationErrors.email && $registrationForm.hasError('email.required')
+							? messageErrors.email.required
+							: ''}
+						{!backValidationErrors.email && $registrationForm.hasError('email.not_an_email')
+							? messageErrors.email.not_an_email
+							: ''}
+						{!backValidationErrors.email &&
+						!$registrationForm.hasError('email.required') &&
+						!$registrationForm.hasError('email.not_an_email')
+							? 'E-mail'
+							: ''}
+					</label>
+				</div>
+			</div>
+
+			<div class="mb-3">
+				<div class="form-floating">
+					<input
+						type="password"
+						class="form-control {!$password.valid && 'is-invalid'}"
+						id="floatingPassword"
+						placeholder="Mot de passe"
+						bind:value={$password.value}
+					/>
+					<label class={!$password.valid && 'text-danger'} for="floatingPassword">
+						{$registrationForm.hasError('password.required') ? messageErrors.password.required : ''}
+						{$registrationForm.hasError('password.between') ? messageErrors.password.between : ''}
+						{!$registrationForm.hasError('password.required') &&
+						!$registrationForm.hasError('password.between')
+							? 'Mot de passe'
+							: ''}
+					</label>
+				</div>
+			</div>
+
+			<div class="mb-3">
+				<div class="form-floating">
+					<input
+						type="password"
+						class="form-control {!$confirmPassword.valid && 'is-invalid'}"
+						id="floatingConfirmPassword"
+						placeholder="Confirmation du mot de passe"
+						bind:value={$confirmPassword.value}
+					/>
+					<label class={!$confirmPassword.valid && 'text-danger'} for="floatingConfirmPassword">
+						{$registrationForm.hasError('confirmPassword.required')
+							? messageErrors.confirmPassword.required
+							: ''}
+						{$registrationForm.hasError('confirmPassword.confirm_password_does_not_match')
+							? messageErrors.confirmPassword.confirm_password_does_not_match
+							: ''}
+						{!$registrationForm.hasError('confirmPassword.required') &&
+						!$registrationForm.hasError('confirmPassword.confirm_password_does_not_match')
+							? 'Confirmation du mot de passe'
+							: ''}
+					</label>
+				</div>
+			</div>
+			<div class="btn-wrapper d-flex justify-content-center mb-4">
+				<button type="submit" disabled={!$registrationForm.valid} class="btn-signup  ">
+					S'inscrire
+				</button>
+			</div>
+		</form>
+		<p class="alreadySignup">
+			Déjà inscrit? <a href="/login" class="text-dark fw-bold"> Se Connecter </a>
+		</p>
+	</div>
 </div>
 
-<form on:submit={handleSubmit}>
-	<div class="mb-3">
-		<div class="form-floating">
-			<input
-				type="text"
-				class="form-control {!$firstName.valid && 'is-invalid'}"
-				id="floatingFirstName"
-				placeholder="Prénom"
-				bind:value={$firstName.value}
-			/>
-			<label class={!$firstName.valid && 'text-danger'} for="floatingFirstName">
-				{$registrationForm.hasError('firstName.required') ? messageErrors.firstName.required : ''}
-				{$registrationForm.hasError('firstName.alpha') ? messageErrors.firstName.alpha : ''}
-				{$registrationForm.hasError('firstName.between') ? messageErrors.firstName.between : ''}
-				{!$registrationForm.hasError('firstName.required') &&
-				!$registrationForm.hasError('firstName.alpha') &&
-				!$registrationForm.hasError('firstName.between')
-					? 'Prénom'
-					: ''}
-			</label>
-		</div>
-	</div>
-	<div class="mb-3">
-		<div class="form-floating">
-			<input
-				type="text"
-				class="form-control {!$lastName.valid && 'is-invalid'}"
-				id="floatingLastName"
-				placeholder="Nom"
-				bind:value={$lastName.value}
-			/>
-			<label class={!$lastName.valid && 'text-danger'} for="floatingLastName">
-				{$registrationForm.hasError('lastName.required') ? messageErrors.lastName.required : ''}
-				{$registrationForm.hasError('lastName.alpha') ? messageErrors.lastName.alpha : ''}
-				{$registrationForm.hasError('lastName.between') ? messageErrors.lastName.between : ''}
-				{!$registrationForm.hasError('lastName.required') &&
-				!$registrationForm.hasError('lastName.alpha') &&
-				!$registrationForm.hasError('lastName.between')
-					? 'Nom'
-					: ''}
-			</label>
-		</div>
-	</div>
-	<div class="mb-3">
-		<div class="form-floating">
-			<input
-				type="email"
-				class="form-control {!$email.valid || backValidationErrors.email ? 'is-invalid' : ''}"
-				id="floatingEmail"
-				placeholder="mail@exemple.fr"
-				bind:value={$email.value}
-				on:input={() => removeBackError('email')}
-			/>
-			<label
-				class={(!$email.valid || backValidationErrors.email) && 'text-danger'}
-				for="floatingEmail"
-			>
-				{backValidationErrors.email ? backValidationErrors.email : ''}
-				{!backValidationErrors.email && $registrationForm.hasError('email.required')
-					? messageErrors.email.required
-					: ''}
-				{!backValidationErrors.email && $registrationForm.hasError('email.not_an_email')
-					? messageErrors.email.not_an_email
-					: ''}
-				{!backValidationErrors.email &&
-				!$registrationForm.hasError('email.required') &&
-				!$registrationForm.hasError('email.not_an_email')
-					? 'E-mail'
-					: ''}
-			</label>
-		</div>
-	</div>
-
-	<div class="mb-3">
-		<div class="form-floating">
-			<input
-				type="password"
-				class="form-control {!$password.valid && 'is-invalid'}"
-				id="floatingPassword"
-				placeholder="Mot de passe"
-				bind:value={$password.value}
-			/>
-			<label class={!$password.valid && 'text-danger'} for="floatingPassword">
-				{$registrationForm.hasError('password.required') ? messageErrors.password.required : ''}
-				{$registrationForm.hasError('password.between') ? messageErrors.password.between : ''}
-				{!$registrationForm.hasError('password.required') &&
-				!$registrationForm.hasError('password.between')
-					? 'Mot de passe'
-					: ''}
-			</label>
-		</div>
-	</div>
-
-	<div class="mb-3">
-		<div class="form-floating">
-			<input
-				type="password"
-				class="form-control {!$confirmPassword.valid && 'is-invalid'}"
-				id="floatingConfirmPassword"
-				placeholder="Confirmation du mot de passe"
-				bind:value={$confirmPassword.value}
-			/>
-			<label class={!$confirmPassword.valid && 'text-danger'} for="floatingConfirmPassword">
-				{$registrationForm.hasError('confirmPassword.required')
-					? messageErrors.confirmPassword.required
-					: ''}
-				{$registrationForm.hasError('confirmPassword.confirm_password_does_not_match')
-					? messageErrors.confirmPassword.confirm_password_does_not_match
-					: ''}
-				{!$registrationForm.hasError('confirmPassword.required') &&
-				!$registrationForm.hasError('confirmPassword.confirm_password_does_not_match')
-					? 'Confirmation du mot de passe'
-					: ''}
-			</label>
-		</div>
-	</div>
-	<button type="submit" disabled={!$registrationForm.valid} class="btn btn-secondary">
-		Créer nouveau compte
-	</button>
-</form>
+<style>
+	.dot i {
+		width: 35px;
+		height: 35px;
+		color: #000;
+		text-align: center;
+	}
+	.btn-signup {
+		background-color: #00aa88;
+		color: #fff;
+		padding: 5px 20px;
+		border-radius: 15px;
+		font-weight: 600;
+		border: none;
+	}
+	.left-banner {
+		background-color: #00aa88;
+		color: #fff;
+		justify-content: center;
+		align-items: center;
+	}
+	.alreadySignup {
+		text-align: right;
+	}
+</style>
