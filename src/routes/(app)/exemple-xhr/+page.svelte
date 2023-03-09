8693e85b1bade0c090e110893d60bc10b04a276c
soukaina <soukaina.bairouk@gmail.com>
2023-03-09 23:39:51 +0100

    api calls xhr & on page load

---
+++ src/routes/(app)/exemple-xhr/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { apiClient } from '$services/apiClient';
+
+	let greetingMessage;
+	async function greetingFromServer() {
+		const response = await apiClient.get(`${import.meta.env.VITE_API_BASE_URL}/hello`, {
+			token: data.accessToken
+		});
+		greetingMessage = response.greeting;
+	}
+	export let data;
+</script>
+
+<div class="container">
+	<p class="exemple">data : {data.accessToken}</p>
+	<button on:click={greetingFromServer}>call api</button>
+	{#if greetingMessage}
+		<p>
+			{greetingMessage}
+		</p>
+	{/if}
+</div>
