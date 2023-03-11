a0a400ccf46d9dafbe2d0b153489703be2dedc9e
mohamedsek <mohamed.sekkoume@gmail.com>
2023-03-11 19:05:09 +0100

    feature/refactor-back-office-statistiques: get GainStats using backOffice service

--- src/routes/(backoffice)/backoffice/stats/+page.svelte
+++ src/routes/(backoffice)/backoffice/stats/+page.svelte
@@ -1,40 +1,43 @@
 <script>
 	import { onMount } from 'svelte';
 	import BarChart from './BarChart.svelte';
+	import backOfficeService from '$lib/services/backOfficeService';
 
-	let DistData;
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	let GainStats = data.GainStats;
 	let chartData;
 	let labels = [];
 	let targetDistribution = [];
 	let currentDistribution = [];
+
 	onMount(async () => {
-		const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/gains/stats`);
-		console.log(response);
+		GainStats = await backOfficeService.getGainStats(data.accessToken);
+		console.log(GainStats);
 
-		DistData = await response.json();
-		console.log(DistData);
-
-		DistData.forEach((element) => {
+		GainStats.forEach((element) => {
 			console.log(element);
 			labels.push(element.title);
 			currentDistribution.push(element.currentDistribution);
 			targetDistribution.push(element.targetDistribution);
 		});
-		chartData = {title:"Statistiques",
+		chartData = {
+			title: 'Statistiques',
 			data: {
 				labels: labels,
 				datasets: [
 					{
 						label: 'current Distribution',
 						data: currentDistribution,
-						backgroundColor: ['rgba(201, 203, 207, 0.5)'],
-						borderColor: ['rgb(201, 203, 207)'],
+						backgroundColor: ['rgba(0, 128, 0, 0.5)'],
+						borderColor: ['rgb(0, 128, 0)'],
 						borderWidth: 1
 					},
 					{
 						label: 'target Distribution',
 						data: targetDistribution,
-						backgroundColor: ['rgba(255, 99, 132, 0.5)'],
+						backgroundColor: ['rgba(255, 153, 0, 0.5)'],
 						borderColor: ['rgb(255, 99, 132)'],
 						borderWidth: 1
 					}
@@ -45,10 +48,9 @@
 </script>
 
 {#if chartData}
-<div class="stats">
-	<BarChart {chartData} />
-</div>
-	
+	<div class="stats">
+		<BarChart {chartData} />
+	</div>
 {/if}
 
 <style>
