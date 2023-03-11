71799aff4ce95f162ed5e9d64d033303fd948d13
mohamedsek <mohamed.sekkoume@gmail.com>
2023-03-11 19:03:23 +0100

    feature/refactor-back-office-statistiques:implement get GainStats at backOffice service

--- src/lib/services/backOfficeService.js
+++ src/lib/services/backOfficeService.js
@@ -1,6 +1,18 @@
 import { apiClient } from '$services/apiClient';
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
+const _getGainStats = async (accessToken) => {
+    let gainStats = await apiClient.get(
+        `${baseUrl}/gains/stats`,
+        { token: accessToken }
+    );
+
+    if (gainStats) {
+        return gainStats;
+    }
+    return [];
+}
+
 const _getJackPot = async (accessToken, payload) => {
     let JackPot = await apiClient.post(
         `${baseUrl}/tickets/jackpot`,
@@ -13,6 +25,7 @@ const _getJackPot = async (accessToken, payload) => {
 }
 
 const backOfficeService = {
+    getGainStats: _getGainStats,
     getJackPot: _getJackPot
 };
 
