5a1a4e8a80549d9bc651145d32f69fd6bc6f9f48
atimzit <atimzit@gmail.com>
2023-03-25 18:17:55 +0100

    feature/seo: fix e2e tests

--- tests/home.test.js
+++ tests/home.test.js
@@ -3,5 +3,5 @@ import { expect, test } from '@playwright/test';
 test('should display the Home Page', async ({ page }) => {
 	// Verify page title
 	await page.goto('/');
-	expect(await page.title()).toBe("Page d'accueil");
+	expect(await page.title()).toBe("ThéTipTop - Accueil");
 });
