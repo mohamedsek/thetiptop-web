5533e0e0a95dca4a07526777ac63966bcd4f525c
mohamedsek <mohamed.sekkoume@gmail.com>
2023-03-31 18:29:24 +0200

    feature/caisse: page layout + access security

--- src/routes/(backoffice)/+layout.server.js
+++ src/routes/(backoffice)/+layout.server.js
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals, request, url }) => {
 	if (locals?.user) {
-		if (locals.user.role.name === 'ADMIN') {
+		if (locals?.user?.role?.name === 'ADMIN') {
 
 		} else {
 			throw redirect(302,'/')
