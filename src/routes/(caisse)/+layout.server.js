2e10248f69ed22dbdeeabb32fc51c93c3af97162
mohamedsek <mohamed.sekkoume@gmail.com>
2023-03-31 18:29:10 +0200

    feature/caisse: page layout + access security

---
+++ src/routes/(caisse)/+layout.server.js
@@ -0,0 +1,16 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals, request, url }) => {
+	if (locals?.user) {
+		if (locals?.user?.role?.name === 'CHECKOUT_MACHINE') { /* empty */ } else {
+			throw redirect(302,'/')
+		}
+	} else {
+		throw redirect(302,'/login')
+	}
+
+	return {
+		user: locals.user,
+		accessToken: locals.accessToken
+	};
+};
---
+++ src/routes/(caisse)/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+    import Footer from "$lib/components/caisse/Footer.svelte";
+    import Header from "$lib/components/caisse/Header.svelte";
+	import CookiePolicy from "$lib/components/CookiePolicy/CookiePolicy.svelte"
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import '$lib/css/reset.css';
+    import '$lib/css/normalize.css';
+    import '$lib/css/app-styles.css';
+
+	export let data;
+</script>
+
+<div class="caisse">
+	<Header props={data}/>
+	<main>
+		<slot />
+	</main>
+	<CookiePolicy/>
+	<Footer />
+</div>
+
+
+<style>
+	.caisse {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
