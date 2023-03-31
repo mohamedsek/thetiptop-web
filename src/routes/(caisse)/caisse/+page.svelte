dcabbb248121e22146b63f6f6144c3b27273795a
mohamedsek <mohamed.sekkoume@gmail.com>
2023-03-31 18:32:06 +0200

    feature/caisse: page caisse - recherche et affichage des gains d'un utilisateur + generation de code ticket

---
+++ src/routes/(caisse)/caisse/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+	import { onMount } from 'svelte';
+	import caisseService from '$lib/services/caisseService.js';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+	let userGains = null;
+	let username = null;
+	let ticket = null;
+
+	async function getUserGains() {
+		userGains = await caisseService.getUserGains(data.accessToken, username);
+	}
+
+	async function getCode() {
+		ticket = await caisseService.getCode(data.accessToken, {});
+	}
+
+	onMount(() => {
+		console.log('Component mounted');
+	});
+</script>
+
+<div class="container text-center">
+	<div>
+		<button class="btn btn-success" on:click={getCode} type="button">Generer un code</button>
+		{#if ticket}
+		<br>
+		{ticket.code}
+		{/if}
+
+	</div>
+	<div class="user">
+		<label for="username">Entrer un nom d'utilisateur:</label>
+		<input type="text" id="username" bind:value={username} />
+		<button class="btn btn-success" on:click={getUserGains} type="button">Chercher</button>
+	</div>
+	{#if userGains}
+		<div class="statstable">
+			<table class="table table-striped .text-bg-success">
+				<thead>
+					<tr>
+						<th scope="col">ticket</th>
+						<th scope="col">gainTitle</th>
+						<th scope="col">Réclamé</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each userGains as gain}
+						<tr>
+							<td>{gain.ticketCode}</td>
+							<td>{gain.gainTitle}</td>
+							<td>{#if gain.isUsed}OUI{:else} En Attente{/if}</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	{/if}
+</div>
+
+<style>
+	.user {
+		height: 200px; /* set the height of the container */
+		align-items: center; /* center items vertically */
+		display: flex;
+		flex-direction: row;
+		font-family: 'Poppins', sans-serif !important;
+		font-weight: 400;
+		font-size: 2ex;
+		text-align: center;
+	}
+</style>
