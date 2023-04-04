5855b31761e99270583582c6cfec1ea25f4661c3
atimzit <at.sb.ms.dsp+atimzit@gmail.com>
2023-04-04 20:41:26 +0200

    Implement Participation page integration test

---
+++ src/lib/mocks/userGainsMock.js
@@ -0,0 +1,30 @@
+
+
+const userGainsMock = [
+    {
+        title: 'title 1 ',
+        imageUrl: 'https://dummyimage.com/640x360/ddd/aaa'
+    },
+    {
+        title: 'title 2',
+        imageUrl: 'https://dummyimage.com/640x360/ddd/aaa'
+    },
+    {
+        title: 'title 3',
+        imageUrl: 'https://dummyimage.com/640x360/ddd/aaa'
+    },
+    {
+        title: 'title 4',
+        imageUrl: 'https://dummyimage.com/640x360/ddd/aaa'
+    },
+    {
+        title: 'title 5',
+        imageUrl: 'https://dummyimage.com/640x360/ddd/aaa'
+    },
+    {
+        title: 'title 6',
+        imageUrl: 'https://dummyimage.com/640x360/ddd/aaa'
+    }
+];
+
+export default userGainsMock;
