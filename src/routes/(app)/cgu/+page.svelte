f859ba63ebc06f8cb10badd6a80347c213a76957
soukaina <soukaina.bairouk@gmail.com>
2023-04-05 11:55:54 +0200

    add CGU page

---
+++ src/routes/(app)/cgu/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+	export let title = "Conditions Générales d'Utilisation";
+</script>
+
+<svelte:head>
+	<title>ThéTipTop - Conditions Générales d'Utilisation</title>
+	<meta
+		name="description"
+		content="Consultez nos conditions générales d'utilisation pour découvrir les règles régissant l'utilisation de notre site TheTipTop. Informations sur les droits et obligations des utilisateurs. En conformité avec la loi française. Consultez maintenant."
+	/>
+</svelte:head>
+<div class="cgu">
+	<h1>{title}</h1>
+	<h2>1. Présentation du site</h2>
+	<p>
+		Le site TheTipTop est un site de vente en ligne de produits destiné aux particuliers et aux
+		professionnels.
+	</p>
+	<h2>2. Conditions d'utilisation</h2>
+	<p>
+		En utilisant le site TheTipTop, vous acceptez les présentes conditions générales d'utilisation.
+	</p>
+	<p>
+		Vous vous engagez à ne pas utiliser le site TheTipTop à des fins illicites ou interdites par les
+		lois en vigueur.
+	</p>
+	<p>
+		TheTipTop se réserve le droit de modifier à tout moment les présentes conditions générales
+		d'utilisation. Il est donc recommandé de les consulter régulièrement.
+	</p>
+	<h2>3. Propriété intellectuelle</h2>
+	<p>
+		Tous les éléments du site TheTipTop sont et restent la propriété intellectuelle et exclusive de
+		TheTipTop. Nul n'est autorisé à reproduire, exploiter, rediffuser ou utiliser à quelque titre
+		que ce soit, même partiellement, des éléments du site qu'ils soient logiciels, visuels ou
+		sonores.
+	</p>
+	<h2>4. Responsabilité</h2>
+	<p>
+		Le site TheTipTop ne saurait être tenu pour responsable des dommages directs ou indirects
+		résultant de l'utilisation de son site web ou d'autres sites qui lui sont liés, notamment en cas
+		d'interruption, de suspension, de modification ou de suppression du site web.
+	</p>
+	<p>
+		TheTipTop ne saurait être tenu pour responsable des dommages directs ou indirects résultant de
+		l'utilisation des produits vendus sur son site web, notamment en cas de non-conformité des
+		produits aux normes en vigueur ou de mauvaise utilisation des produits.
+	</p>
+</div>
+
+<style>
+	.cgu {
+		max-width: 800px;
+		margin: 0 auto;
+		padding: 40px;
+		color: #333;
+	}
+
+	.cgu h1 {
+		font-size: 32px;
+		font-weight: bold;
+		color: #333;
+		text-align: center;
+		margin-bottom: 30px;
+	}
+
+	.cgu h2 {
+		font-size: 24px;
+		font-weight: bold;
+		color: #333;
+		margin-top: 30px;
+		margin-bottom: 20px;
+	}
+
+	.cgu p {
+		font-size: 16px;
+		line-height: 1.5;
+		color: #333;
+		text-align: justify;
+		margin-bottom: 20px;
+	}
+</style>
