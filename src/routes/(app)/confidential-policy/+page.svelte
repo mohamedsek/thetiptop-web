2c5a15b27b5cd4513bebd4e939dbb10e58ed1d32
soukaina <soukaina.bairouk@gmail.com>
2023-04-05 11:56:53 +0200

    add condidential policy page

---
+++ src/routes/(app)/confidential-policy/+page.svelte
@@ -0,0 +1,89 @@
+<script>
+	export let title = 'Politique de confidentialité';
+</script>
+
+<svelte:head>
+	<title>ThéTipTop - Politique de confidentialité</title>
+	<meta
+		name="description"
+		content="Consultez notre politique de confidentialité pour en savoir plus sur la manière dont nous traitons vos données personnelles sur TheTipTop. Nous respectons votre vie privée et nous engageons à protéger vos informations. Découvrez nos engagements en matière de confidentialité."
+	/>
+</svelte:head>
+<div class="confidens">
+	<h1>{title}</h1>
+	<p>
+		Cette politique de confidentialité décrit la manière dont TheTipTop collecte, utilise et protège
+		les informations que vous nous fournissez lorsque vous utilisez notre site web.
+	</p>
+	<h2>Collecte d'informations</h2>
+	<p>
+		Nous pouvons collecter des informations personnelles telles que votre nom, votre adresse e-mail
+		et votre adresse postale lorsque vous vous inscrivez sur notre site, passez une commande ou
+		remplissez un formulaire.
+	</p>
+	<p>
+		Nous collectons également des informations sur votre utilisation de notre site web via des
+		cookies et des technologies similaires. Ces informations peuvent inclure votre adresse IP, votre
+		type de navigateur, les pages que vous consultez et les actions que vous effectuez sur notre
+		site web.
+	</p>
+	<h2>Utilisation des informations</h2>
+	<p>
+		Nous utilisons les informations que nous collectons pour vous fournir les services que vous
+		demandez, traiter vos commandes, améliorer notre site web et communiquer avec vous.
+	</p>
+	<p>
+		Nous pouvons également utiliser les informations pour personnaliser votre expérience sur notre
+		site web et vous présenter des offres spéciales et des promotions.
+	</p>
+	<h2>Protection des informations</h2>
+	<p>
+		Nous prenons la protection de vos informations personnelles très au sérieux et nous avons mis en
+		place des mesures de sécurité appropriées pour empêcher tout accès non autorisé, divulgation,
+		modification ou destruction de vos informations personnelles.
+	</p>
+	<h2>Partage des informations</h2>
+	<p>
+		Nous ne partageons pas vos informations personnelles avec des tiers à des fins de marketing ou
+		de publicité.
+	</p>
+	<p>
+		Nous pouvons partager vos informations avec des tiers qui fournissent des services pour notre
+		site web, tels que des fournisseurs de paiement et des entreprises de livraison.
+	</p>
+	<h2>Modifications de la politique de confidentialité</h2>
+	<p>
+		Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Les
+		modifications seront publiées sur cette page et entreront en vigueur immédiatement après leur
+		publication.
+	</p>
+	<h2>Contactez-nous</h2>
+	<p>
+		Si vous avez des questions ou des préoccupations concernant cette politique de confidentialité,
+		n'hésitez pas à nous contacter à l'adresse e-mail suivante : privacy@thetiptop.com
+	</p>
+</div>
+
+<style>
+	.confidens {
+		max-width: 800px;
+		margin: 0 auto;
+		padding: 40px;
+		color: #333;
+	}
+
+	.confidens h1 {
+		font-size: 32px;
+		font-weight: bold;
+		color: #333;
+		text-align: center;
+		margin-bottom: 30px;
+	}
+
+	.confidens p {
+		font-size: 16px;
+		line-height: 1.5;
+		color: #333;
+		text-align: justify;
+	}
+</style>
